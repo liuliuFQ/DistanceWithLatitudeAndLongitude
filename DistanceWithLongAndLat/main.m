5750aa86d2d328b9c678c66236ff9ec69ad4d8e8
Ibokan <ibokan@12-07.local>
2014-05-19 11:11:56 +0800

    Initial Commit

---
+++ DistanceWithLongAndLat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DistanceWithLongAndLat
+//
+//  Created by Ibokan on 14-5-19.
+//  Copyright (c) 2014年 CainiaoLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
